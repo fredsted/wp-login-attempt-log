5d4abe09c67870dc9da157d33d7a01343fbc23b4
Simon Fredsted <simon@fredsted.me>
2014-11-04 17:54:08 +0100

    Version bump

--- login-attempt-log.php
+++ login-attempt-log.php
@@ -2,7 +2,7 @@
 /* 
 Plugin Name: Login Attempt Log 
 Plugin URI: http://www.simonfredsted.com/wordpress/login-attempt-log
-Version: 1.0 
+Version: 1.1
 Author: Simon Fredsted
 Description: Logs login attempts to your WordPress site.
 */
