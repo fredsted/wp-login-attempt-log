568ed6d5302ccd86a11e947b5523c386a69a3d77
Simon Fredsted <simon@fredsted.me>
2014-11-04 18:12:57 +0100

    Only show graphs and boxes when there's enough data available

--- templates/lal-settings.tpl.php
+++ templates/lal-settings.tpl.php
@@ -3,72 +3,93 @@
 
 <h3>Last 14 days</h3>
 
-<script type="text/javascript">
-	window.onload = function () {
-		var chart = new CanvasJS.Chart("chartContainer", {
-			backgroundColor: "#F1F1F1",
-			toolTipContent: "attempts: {y} ",
-			data: [{      
-				toolTipContent: "{y} attempts",  
-				type: "column",
-				dataPoints: <?= json_encode($chart) ?>
-			}],
-		});
-		
-		chart.render();
-	}
-</script>
 
-<div id="chartContainer" style="height: 300px; width: <?= count($chart)*50 ?>px;"></div>
+	<?php if (!$counts): ?>
 
-<div id="lal-quickstats">
+		<p>
+			<i>Unfortunately, there is no data to show yet since the plugin was just installed.</i>
+		</p>
 
-	<?php
-	/*
-	 * This will display interesting statistics, but there's no need
-	 * to display the same number multiple times (which will happen in the 
-	 * first couple days and weeks)
-	 */
-  	?>
-	<div class="lal-qs-box" id="lal-qs-total">
-		<div class="lal-qs-amount"><?=$counts->total?></div>
-		<div class="lal-qs-time">Grand total attempts</div>
-	</div>  	
-	<?php if($counts->month != $counts->total): ?>
-	<div class="lal-qs-box" id="lal-qs-month">
-		<div class="lal-qs-amount"><?=$counts->month?></div>
-		<div class="lal-qs-time">This month</div>
-	</div>
-	<?php endif; ?>
-	<?php if($counts->week != $counts->total): ?>
-	<div class="lal-qs-box" id="lal-qs-week">
-		<div class="lal-qs-amount"><?=$counts->week?></div>
-		<div class="lal-qs-time">This week</div>
-	</div>
-	<?php endif; ?>
-	<?php if($counts->day != $counts->total): ?>
-	<div class="lal-qs-box" id="lal-qs-day">
-		<div class="lal-qs-amount"><?=$counts->day?></div>
-		<div class="lal-qs-time">Today</div>
-	</div>
-	<?php endif; ?>
-	<?php if (($counts->month+$counts->week+$counts->day) != ($counts->average_per_week+$counts->average_per_day+$counts->average_per_month)): ?>
-	
-	<br style="clear:both;">
-	<?php if($counts->average_per_month != $counts->total): ?>
-	<div class="lal-qs-box" id="lal-qs-avg">
-		<div class="lal-qs-amount"><?=round($counts->average_per_month,1)?></div>
-		<div class="lal-qs-time">Per month (average)</div>
-	</div>
-	<?php endif; ?>		
-	<div class="lal-qs-box" id="lal-qs-avg">
-		<div class="lal-qs-amount"><?=round($counts->average_per_week,1)?></div>
-		<div class="lal-qs-time">Per week (average)</div>
-	</div>	
-	<div class="lal-qs-box" id="lal-qs-avg">
-		<div class="lal-qs-amount"><?=round($counts->average_per_day,1)?></div>
-		<div class="lal-qs-time">Per day (average)</div>
-	</div>
+	<?php else: ?>
+
+		<?php if (count($chart) > 1): ?>
+
+			<script type="text/javascript">
+				window.onload = function () {
+					var chart = new CanvasJS.Chart("chartContainer", {
+						backgroundColor: "#F1F1F1",
+						toolTipContent: "attempts: {y} ",
+						data: [{
+							toolTipContent: "{y} attempts",
+							type: "column",
+							dataPoints: <?= json_encode($chart) ?>
+						}]
+					});
+
+					chart.render();
+				}
+			</script>
+
+			<div id="chartContainer" style="height: 300px; width: <?= count($chart)*50 ?>px;"></div>
+
+		<?php else: ?>
+
+			<p>
+				<i>The graph will be shown when there's more data available.</i>
+			</p>
+
+		<?php endif; ?>
+
+		<div id="lal-quickstats">
+
+		<?php
+		/*
+		 * This will display interesting statistics, but there's no need
+		 * to display the same number multiple times (which will happen in the
+		 * first couple days and weeks)
+		 */
+	    ?>
+		<div class="lal-qs-box" id="lal-qs-total">
+			<div class="lal-qs-amount"><?=$counts->total?></div>
+			<div class="lal-qs-time">Grand total attempts</div>
+		</div>
+		<?php if($counts->month != $counts->total): ?>
+		<div class="lal-qs-box" id="lal-qs-month">
+			<div class="lal-qs-amount"><?=$counts->month?></div>
+			<div class="lal-qs-time">This month</div>
+		</div>
+		<?php endif; ?>
+		<?php if($counts->week != $counts->total): ?>
+		<div class="lal-qs-box" id="lal-qs-week">
+			<div class="lal-qs-amount"><?=$counts->week?></div>
+			<div class="lal-qs-time">This week</div>
+		</div>
+		<?php endif; ?>
+		<?php if($counts->day != $counts->total): ?>
+		<div class="lal-qs-box" id="lal-qs-day">
+			<div class="lal-qs-amount"><?=$counts->day?></div>
+			<div class="lal-qs-time">Today</div>
+		</div>
+		<?php endif; ?>
+		<?php if (($counts->month+$counts->week+$counts->day) != ($counts->average_per_week+$counts->average_per_day+$counts->average_per_month)): ?>
+
+		<br style="clear:both;">
+		<?php if($counts->average_per_month != $counts->total): ?>
+		<div class="lal-qs-box" id="lal-qs-avg">
+			<div class="lal-qs-amount"><?=round($counts->average_per_month,1)?></div>
+			<div class="lal-qs-time">Per month (average)</div>
+		</div>
+		<?php endif; ?>
+		<div class="lal-qs-box" id="lal-qs-avg">
+			<div class="lal-qs-amount"><?=round($counts->average_per_week,1)?></div>
+			<div class="lal-qs-time">Per week (average)</div>
+		</div>
+		<div class="lal-qs-box" id="lal-qs-avg">
+			<div class="lal-qs-amount"><?=round($counts->average_per_day,1)?></div>
+			<div class="lal-qs-time">Per day (average)</div>
+		</div>
+		<?php endif; ?>
+
 	<?php endif; ?>
 	<br />
 </div>
